4cb5e56ee81bb8374d57ea847a5552b7d435c9e1
guda-radhika <gudaradhika4@gmail.com>
2024-11-22 12:24:40 +0530

    Add files via upload
    multiplication table

---
+++ multiply.py
@@ -0,0 +1,9 @@
+while True:
+   x=int(input("enter a number"))
+   if(x==0):
+          print("thank you")
+          break
+   else:
+        for i in range(1,11):
+           print(x,"*",i,"=",x*i)
+      
